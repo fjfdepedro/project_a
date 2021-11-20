e8714587d4fec8eaf5eddac5e661ce8524ca7cb0
fjfdepedro <fjfdepedro@gmail.com>
2021-11-20 17:15:59 +0100

    fix type field fee_configurations

--- db/migrate/2021120105932_create_fee_configurations.rb
+++ db/migrate/2021120105932_create_fee_configurations.rb
@@ -2,9 +2,9 @@ class CreateFeeConfigurations < ActiveRecord::Migration[6.0]
   def change
     create_table :fee_configurations do |t|
       t.references :fee_configurations_type, foreign_key: true, null: false
-      t.string :fee_percentage
-      t.string :min_amount
-      t.string :max_amount
+      t.float :fee_percentage
+      t.integer :min_amount
+      t.integer :max_amount
       t.timestamps
     end
   end
