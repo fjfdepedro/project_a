ed9e4eee942ba5b7b9b81266ab97c9291355fca1
fjfdepedro <fjfdepedro@gmail.com>
2021-11-20 14:39:24 +0100

    fix call validate cid in merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,6 @@
 class Merchant < ActiveRecord::Base
   validates :email, presence: true, email: true
-  validates :cif, presence: true, cif: true
+  validates :cif, presence: true, nif: true
 
   has_many :orders
   belongs_to :fee_configurations_type
