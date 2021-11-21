5f76166708477b5c9f6b9bddfa0fd53018c311cc
fjfdepedro <fjfdepedro@gmail.com>
2021-11-21 21:31:16 +0100

    fix relations models disbursement, fee_configuration and order

--- app/models/disbursement.rb
+++ app/models/disbursement.rb
@@ -3,4 +3,13 @@ class Disbursement < ActiveRecord::Base
   belongs_to :fee_configuration
 
   attr_accessor :order, :fee_configuration
+
+  scope :find_by_merchant, (lambda do |merchant_id|
+    joins(:merchant).where(id: merchant_id)
+  end)
+
+  scope :find_by_day_week, (lambda do |day_week|
+    where('from_date < ? AND to_date > ?', day_week, day_week + 6.days)
+  end)
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,8 @@ class Order < ActiveRecord::Base
   belongs_to :shopper
   has_one :disbursement
 
+  scope :find_by_day_week, (lambda do |day_week|
+    where('from_date < ? AND to_date > ?', day_week, day_week + 6.days)
+  end)
+
 end
